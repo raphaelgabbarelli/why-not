7f6b61c630be1e18b2ce2e11281f34ad2833219f
Michal Bryxí <michal.bryxi@gmail.com>
2015-11-07 01:03:31 +0100

    Sending random numbers

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var _ = require('lodash');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -11,12 +12,18 @@ app.use('/lib', express.static('lib'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+
+  var interval = setInterval(function () {
+    var x = _.random(1, 100),
+        y = _.random(1, 100),
+        z = _.random(1, 100);
+
+    socket.emit("msg", {'x': x, 'y': y, 'z': z});
+  }, 1000);
 });
 
 http.listen(3000, function(){
